06f7430db86ccbe9ca7b4d3b696b0bedfaa0f56b
VMB-DEV <vkdev7360@gmail.com>
2025-11-06 08:50:59 +0100

    get current overflow state

--- printer.zig
+++ printer.zig
@@ -98,7 +98,7 @@ pub const Printer = struct {
         std.debug.print("\x1b[s", .{}); // Save cursor position
         std.debug.print("\x1b[1;1H", .{}); // Move to second line, first column
         std.debug.print("\x1b[K", .{}); // Clear the line
-        std.debug.print("w{}-c{} : ", .{ word_idx, char_idx });
+        std.debug.print("w{}-c{}-o{}: ", .{ word_idx, char_idx,  self.words_state_ptr.word_states[word_idx].getFilledOverFlowLen()});
         for (self.words_state_ptr.word_states, 0..) |*word_state_val, w_idx| {
             std.debug.print("[{}, {}, {}, {}]  ", .{ w_idx, word_state_val.word_slice.len, word_state_val.getLastCharIdxToFill(), word_state_val.getFilledOverFlowLen() });
         }
